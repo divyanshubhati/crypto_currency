92e752b8aa5c330b9d3068bc7459094f8e7311c5
dbhati <divyanshu.bhati@oracle.com>
2020-09-26 18:31:55 +0530

    👻👻 Add user cartoons 👻 👻

--- src/js/index.js
+++ src/js/index.js
@@ -49,6 +49,10 @@ function init () {
   $(".list-group").append(blockCardListElement);
 }
 
+
+
+
+
 $("#send_money").on("click", function() {
   const from = $("#inputFrom").val();
   const to = $("#inputTo").val();
@@ -70,6 +74,11 @@ $("#send_money").on("click", function() {
     throw new Error('Transaction must include from and to address');
   }
 
+  if(transaction.fromAddress == transaction.toAddress) {
+    alert("Internal transfer is not allowed");
+    throw new Error('Internal transfer is not allowed');
+  }
+
   if(!transaction.isValid()) {
     throw new Error('Cannot add invalid transaction to chain');
   }
@@ -82,7 +91,7 @@ $("#send_money").on("click", function() {
     throw new Error('Insufficient funds ');
   }
   fromUser.balance -= transaction.amount;
-  toUser.balance = toUser.balance + transaction.amount; 
+  toUser.balance = parseInt(toUser.balance) + parseInt(transaction.amount); 
   pending_transactions.push(transaction);
   $("#bob_balance").text(bob.balance);
   $("#alice_balance").text(alice.balance);
@@ -96,11 +105,23 @@ $("#send_money").on("click", function() {
   </tr>`
   $("#pending_transactions").append(transaction_row);
 
+  $([document.documentElement, document.body]).animate({
+    scrollTop: $("#user-info").offset().top
+  }, 1500);
+
 });
+
+
+
 
 function getLatestBlockHash() {
   return blockChain[blockChain.length - 1].hash;
 }
+
+
+
+
+
 
 $("#mine_block").on("click", function(){
   if (pending_transactions.length == 0 ) {
@@ -153,6 +174,9 @@ $("#mine_block").on("click", function(){
   $(".transaction_row").addClass("table-success");
 
   $(".list-group").append(blockCard);
+  $([document.documentElement, document.body]).animate({
+    scrollTop: $("#block-data").offset().top
+  }, 1500);
 });
 
 function checkChainValidity(){
@@ -170,20 +194,32 @@ function checkChainValidity(){
   return true
 }
 
-$("#temper_data").on("click", function(){
+$("#tamper_data").on("click", function(){
+  if (mined_transaction.length == 0 ) {
+    alert("No Mined traction present. Add a transaction and mine the block before tampering the data.")
+    return;
+  }
   const transaction_id = $("#transaction_id").val();
+  if(transaction_id >= mined_transaction.length) {
+    alert("The selected transaction is either not mined yet or not present")
+    return;
+  }
   const new_amount = $("#new_amount").val();
 
   mined_transaction[transaction_id].amount = new_amount;
   if (checkChainValidity()) {
-    $(".transaction_row").removeClass("table-danger");
-    $(".transaction_row").addClass("table-success");
+    $(".table-danger").addClass("table-success");
+    $(".table-danger").removeClass("table-danger");
     $(".list-group-item").removeClass("bg-danger");
   } else {
-    $(".transaction_row").removeClass("table-success");
-    $(".transaction_row").addClass("table-danger");
+    $(".table-success").addClass("table-danger");
+    $(".table-success").removeClass("table-success");
     $(".list-group-item").addClass("bg-danger");
   }
+
+  $([document.documentElement, document.body]).animate({
+    scrollTop: $("#block-data").offset().top
+  }, 1500);
 
 });
 
